71f073e41489a23dd05d73017cb502e30c623e7f
Ludovic Claude <ludovic.claude54@gmail.com>
2015-11-30 10:39:56 +0100

    Update workflow and summary-stats

--- setup-federation.sh
+++ setup-federation.sh
@@ -4,10 +4,8 @@ DATACENTER=federation
 REMOTE_USER=$USER
 
 ./common/scripts/bootstrap.sh
- # TODO: remove skip tags docker-registry
 ansible-playbook --ask-become-pass -i envs/$DATACENTER/etc/ansible/ \
         -e play_dir=$(pwd) \
         -e docker_registry_config_orig_dir=$(pwd)/envs/federation \
         -e datacenter=$DATACENTER \
-        --skip-tags=front,docker-registry \
         install.yml
