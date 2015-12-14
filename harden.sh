ed731eb1ffe6efe095b8c087b250a2e36c7b2aeb
Ludovic Claude <ludovic.claude54@gmail.com>
2015-12-14 13:41:37 +0100

    Use upstream code for maven role

--- harden.sh
+++ harden.sh
@@ -2,6 +2,9 @@
 
 echo "Harden the managed servers"
 
+# Reference:
+# http://geekeasier.com/protect-ssh-server-with-installing-fail2ban-on-linuxubuntu/3774/
+
 DATACENTER=main
 
 ansible-playbook --ask-become-pass -i envs/$DATACENTER/etc/ansible.security/ \
