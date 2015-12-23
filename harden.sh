7d056311913e4772acfd35d6cf363a819e2d68c6
Ludovic Claude <ludovic.claude54@gmail.com>
2015-12-23 19:27:35 +0100

    Security hardening and improve gitlab setup

--- harden.sh
+++ harden.sh
@@ -5,6 +5,15 @@ echo "Harden the managed servers"
 # Reference:
 # http://geekeasier.com/protect-ssh-server-with-installing-fail2ban-on-linuxubuntu/3774/
 
+[ -d .git-crypt ] || (
+    git crypt init
+    git-crypt add-gpg-user ludovic.claude@chuv.ch
+    echo "Setup .gitattributes, see https://www.agwa.name/projects/git-crypt/"
+    echo "Check that your files are well protected using"
+    echo "  git check-attr -a <path to file to encrypt>"
+    echo "The file should have diff: git-crypt and filter: git-crypt present"
+)
+
 DATACENTER=main
 
 ansible-playbook --ask-become-pass -i envs/$DATACENTER/etc/ansible.security/ \
