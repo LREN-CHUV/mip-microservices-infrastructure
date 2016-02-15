9ed65855925e2da7b3f56b51ba155843fcd6f506
Ludovic Claude <ludovic.claude54@gmail.com>
2016-02-15 22:58:29 +0100

    Update docker and compose

--- common/scripts/fix-mesos-cluster.sh
+++ common/scripts/fix-mesos-cluster.sh
@@ -17,7 +17,7 @@ rm -f /var/log/mesos/*
 rm -rf /tmp/mesos/ /var/run/mesos/ /var/mesos
 
 # Extreme cases: reset zookeeper
-#rm -rf /var/lib/zookeeper
+#rm -rf /var/lib/zookeeper/*
 
 service zookeeper start
 service mesos-master start
