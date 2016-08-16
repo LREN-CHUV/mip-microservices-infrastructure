5406e6ccffb0189ba41e9c6e1f35084955fe7c48
Ludovic Claude <ludovic.claude54@gmail.com>
2016-08-16 16:19:04 +0200

    Improve fix-mesos-cluster script: fix whole cluster at once

--- common/scripts/fix-mesos-cluster.sh
+++ common/scripts/fix-mesos-cluster.sh
@@ -1,28 +1,39 @@
-#!/bin/sh
+#!/bin/bash
+#
+# Attempts to fix the Mesos cluster by cleaning up logs and restarting all services
+# There is an option (--reset) that can destroy all state in the cluster (= the list of running application
+# and their current status) in an attempt to recover the clsuter after some catastrophic failure
+# or loss of coherence (phantom tasks...)
 
-# Use those commands if Chronos doesn't restart after an upgrade
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
 
-service marathon stop
-service mesos-slave stop
-docker rm docker-registry
-service docker stop
-service mesos-master stop
-service zookeeper stop
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     cd -P "$( dirname "$SOURCE" )"
+     pwd
+}
 
-rm -f /var/lib/mesos/meta/slaves/latest
-rm -f /var/log/mesos/*
+ROOT=$(get_script_dir)
 
-# Brute force
-rm -rf /var/run/mesos/ /var/mesos /var/lib/mesos/*
+DATACENTER=federation
 
-# Extreme cases: reset zookeeper
-#rm -rf /var/lib/zookeeper/*
-#rm -rf /var/log/zookeeper/*
+OPTS=""
+if [ "$1" = "--reset" ]; then
+  OPTS="-e reset_mesos=True"
+  echo "This action will destroy all applications in your cluster."
+  echo "You will have to reinstall all software by running:"
+  echo "    ./setup.sh --tags=marathon-app"
+  echo
+  echo "Please manually stop all applications using the Marathon interface before continuing"
+  echo "Press enter to continue."
+  read -p "> " go
+fi
 
-service zookeeper start
-service mesos-master start
-sleep 5
-service docker start
-service mesos-slave start
-sleep 5
-service marathon start
+ansible-playbook --ask-become-pass -i envs/$DATACENTER/etc/ansible/ \
+        -e play_dir=$ROOT/../.. \
+        -e datacenter=$DATACENTER $OPTS \
+        $ROOT/../playbooks/fix-mesos-cluster.yml
