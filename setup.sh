e18f0a1da5fceb57120586078c4ceb7726482d4f
Ludovic Claude <ludovic.claude54@gmail.com>
2016-11-04 21:15:43 +0100

    Add lib_roles_path variable to ansible-playbook

--- setup.sh
+++ setup.sh
@@ -6,5 +6,6 @@ DATACENTER=federation
 ./common/scripts/bootstrap.sh
 ansible-playbook --ask-become-pass -i envs/$DATACENTER/etc/ansible/ \
         -e play_dir=$(pwd) \
+        -e lib_roles_path="$(pwd)/roles" \
         -e datacenter=$DATACENTER $@ \
         install.yml
