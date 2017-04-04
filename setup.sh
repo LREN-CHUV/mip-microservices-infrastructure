0bb3c14a3b116793fa53a9f805cc995607119f0a
Ludovic Claude <ludovic.claude54@gmail.com>
2017-04-04 15:45:55 +0200

    Ensure deployment runs from well versioned project sources

--- setup.sh
+++ setup.sh
@@ -3,6 +3,24 @@ set -e
 
 DATACENTER=federation
 
+force=false
+if [ "$1" = "--force" ]; then
+  force=true
+  shift
+fi
+
+count=$(git status --porcelain | wc -l)
+if test "$count" -gt 0; then
+  git status
+  if $force; then
+    echo "WARNING: Not all files have been committed in Git."
+    echo "Will continue as --force is with you"
+  else
+    echo "Not all files have been committed in Git. Release aborted"
+    exit 1
+  fi
+fi
+
 ./common/scripts/bootstrap.sh
 ansible-playbook --ask-become-pass -i envs/$DATACENTER/etc/ansible/ \
         -e play_dir="$(pwd)" \
