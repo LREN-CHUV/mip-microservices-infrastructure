80746ee6aa72fdb3bd1b7061673bb6b2f2c04816
Ludovic Claude <ludovic.claude54@gmail.com>
2017-05-01 21:19:59 +0200

    Improvements

--- demo/frontend/portal/run.sh
+++ demo/frontend/portal/run.sh
@@ -3,10 +3,10 @@
 set -e
 
 META_DB_PROJECT=${META_DB_PROJECT:-~/Workspace/GitLab/mip-private/meta-db}
-SCIENCE_DB_PROJECT=${SCIENCE_DB_PROJECT:-~/Workspace/GitLab/mip-private/adni-merge-db}
+FEATURES_DB_PROJECT=${FEATURES_DB_PROJECT:-~/Workspace/GitLab/mip-private/adni-merge-db}
 
 rsync -ar --delete --exclude=".keep" --exclude=".git" "$META_DB_PROJECT/" src/meta-db/
-rsync -ar --delete --exclude=".keep" --exclude=".git" "$SCIENCE_DB_PROJECT/" src/research-db/
+rsync -ar --delete --exclude=".keep" --exclude=".git" "$FEATURES_DB_PROJECT/" src/research-db/
 
 if [ -z "$@" ]; then
   vagrant up
