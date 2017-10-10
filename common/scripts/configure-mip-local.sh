41ae0436b2f077941989e03cb9afdef18c668d43
Ludovic Claude <ludovic.claude54@gmail.com>
2017-10-10 18:33:47 +0200

    Modular installation

--- common/scripts/configure-mip-local.sh
+++ common/scripts/configure-mip-local.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+debug=false
+if [ "$1" == "--debug" ]; then
+  debug=true
+fi
+
 get_script_dir () {
      SOURCE="${BASH_SOURCE[0]}"
 
@@ -89,18 +94,36 @@ do
   break
 done
 
+declare -A mip_building_blocks
+# shellcheck disable=SC2154
+{
+  mip_building_blocks["dc"]=true
+  mip_building_blocks["df"]=true
+  mip_building_blocks["af"]=true
+  mip_building_blocks["hdb"]=true
+  mip_building_blocks["ref"]=true
+  mip_building_blocks["wa"]=true
+}
+
 echo
 echo "Which components of MIP Local do you want to install?"
 PS3='> '
-options=("All" "No Data Factory")
+options=("All" "Web analytics and databases only" "Data Factory only")
 select building_blocks in "${options[@]}";
 do
   case "$building_blocks" in
     "All")
-        ANSIBLE_OPTS="$ANSIBLE_OPTS -e mip_building_blocks=all"
         ;;
-    "No Data Factory")
-        ANSIBLE_OPTS="$ANSIBLE_OPTS -e mip_building_blocks=!df"
+    "Web analytics and databases only")
+        unset mip_building_blocks['dc']
+        unset mip_building_blocks['df']
+        ;;
+    "Data Factory only")
+        unset mip_building_blocks["dc"]
+        unset mip_building_blocks["af"]
+        unset mip_building_blocks["hdb"]
+        unset mip_building_blocks["ref"]
+        unset mip_building_blocks["wa"]
         ;;
     *)
         echo invalid option
@@ -110,42 +133,108 @@ do
   break
 done
 
-if [[ "$location" == "This machine" && "$building_blocks" == "All" && ! -d /usr/local/MATLAB/2016b ]]; then
-    echo "Is Matlab 2016b installed on this machine?"
-    PS3="> "
-    options=("yes" "no")
-    select matlab_installed in "${options[@]}";
-    do
-      case "$matlab_installed" in
-          "yes")
-              echo "Enter the root of Matlab installation, e.g. /opt/MATLAB/2016b :"
-              read -p "path > " matlab_root
-              ANSIBLE_OPTS="$ANSIBLE_OPTS -e matlab_root=$matlab_root"
-              ;;
-          "no")
-              SETUP_ANSIBLE_OPTS="$SETUP_ANSIBLE_OPTS --skip-tags=spm"
-              echo "The Data Factory will be installed but image pre-processing will not be functional."
-              echo "When Matlab is available, please run again the installation to setup Matlab and SPM using:"
-              echo "  ./setup.sh --tags=data-factory"
-              echo
-              ;;
-            *)
-              echo invalid option
-              exit 1
-              ;;
-      esac
-      break
-    done
+if [[ "${mip_building_blocks['hdb']}" == "true" ]]; then
+  echo
+  echo "Do you want to store research-grade data in CSV files or in a relational database?"
+  PS3='> '
+  options=("CSV files" "Relational database")
+  select building_blocks in "${options[@]}";
+  do
+    case "$building_blocks" in
+      "CSV files")
+          ANSIBLE_OPTS="$ANSIBLE_OPTS -e features_from=ldsm-db"
+          ;;
+      "Relational database")
+          unset mip_building_blocks['hdb']
+          ANSIBLE_OPTS="$ANSIBLE_OPTS -e features_from=research-db"
+          ;;
+      *)
+          echo invalid option
+          exit 1
+          ;;
+    esac
+    break
+  done
 fi
+
+if [[ "${mip_building_blocks['df']}" == "true" ]]; then
+
+  if [[ "$location" == "This machine" && ! -d /usr/local/MATLAB/2016b ]]; then
+      echo "Is Matlab 2016b installed on this machine?"
+      PS3="> "
+      options=("yes" "no")
+      select matlab_installed in "${options[@]}";
+      do
+        case "$matlab_installed" in
+            "yes")
+                echo "Enter the root of Matlab installation, e.g. /opt/MATLAB/2016b :"
+                read -p "path > " matlab_root
+                ANSIBLE_OPTS="$ANSIBLE_OPTS -e matlab_root=$matlab_root"
+                ;;
+            "no")
+                SETUP_ANSIBLE_OPTS="$SETUP_ANSIBLE_OPTS --skip-tags=spm"
+                echo "The Data Factory will be installed but image pre-processing will not be functional."
+                echo "When Matlab is available, please run again the installation to setup Matlab and SPM using:"
+                echo "  ./setup.sh --tags=data-factory"
+                echo
+                ;;
+              *)
+                echo invalid option
+                exit 1
+                ;;
+        esac
+        break
+      done
+  fi
+
+  # TODO: Slack token and channel
+
+fi
+
+if [[ "${mip_building_blocks['wa']}" == "true" ]]; then
+  echo "Do you want to secure access to the local MIP Web portal?"
+  PS3="> "
+  options=("yes" "no")
+  select portal_security in "${options[@]}";
+  do
+    case "$portal_security" in
+        "yes")
+            echo "To enable portal security, please enter the HBP Client ID and Client secret"
+            read -p "HBP Client ID > " hbp_client_id
+            read -p "HBP Client secret > " hbp_client_secret
+            if [[ -n "$hbp_client_id" && -n "$hbp_client_secret" ]]; then
+              ANSIBLE_OPTS="$ANSIBLE_OPTS -e portal_backend_hbp_client_id=$hbp_client_id -e portal_backend_hbp_client_secret=$hbp_client_secret"
+            else
+              echo "You did not filled all required information. Security is disabled"
+            fi
+            ;;
+        "no")
+            ;;
+          *)
+            echo invalid option
+            exit 1
+            ;;
+    esac
+    break
+  done
+
+  echo "To enable Google analytics, please enter the Google tracker ID or leave this blank to disable it"
+  read -p "Google tracker ID > " google_tracker_id
+  ANSIBLE_OPTS="$ANSIBLE_OPTS -e portal_frontend_google_tracker_id=$google_tracker_id"
+fi
+
+ANSIBLE_OPTS="$ANSIBLE_OPTS -e mip_building_blocks=$(echo "${!mip_building_blocks[@]}" | tr ' ' ',')"
 
 echo
 echo "Generating the configuration for MIP Local..."
-echo "Additional informations will be asked, including a long list of passwords for the databases."
-echo "Please keep this information safe."
 echo
 if [ "$sudo_pwd" == "yes" ]; then
   echo "Please enter the sudo password for the target server"
   echo
+fi
+
+if [ "$debug" == "true" ]; then
+  set -x
 fi
 
 # shellcheck disable=SC2086
@@ -155,6 +244,8 @@ ansible-playbook $ANSIBLE_OPTS \
         -e lib_roles_path="$(pwd)/roles" \
         -e datacenter="mip-local" \
         common/playbooks/generate-mip-local-config.yml
+
+set +x
 
 echo "DATACENTER=mip-local" > .environment
 
