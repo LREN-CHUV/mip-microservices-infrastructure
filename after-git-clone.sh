7e2d50301e03cdd0b4fdf3a0b0a669b4b3c8ba87
Ludovic Claude <ludovic.claude54@gmail.com>
2017-10-13 15:48:11 +0200

    Optional execution of pre-commit, not needed for projects derived from this template project

--- after-git-clone.sh
+++ after-git-clone.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 git submodule update --init --recursive
 if [ -x /usr/local/bin/pre-commit ]; then
--- after-update.sh
+++ after-update.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 git submodule sync
 git submodule update --init
