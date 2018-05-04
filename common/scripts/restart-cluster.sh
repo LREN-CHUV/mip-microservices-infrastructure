d5bc320c04813c05cdd8437ad1cc1ef8b5f5063f
Ludovic Claude <ludovic.claude@laposte.net>
2018-05-05 01:10:44 +0200

    Improve configuration and troubleshooting of Mesos in a cluster

--- common/scripts/restart-cluster.sh
+++ common/scripts/restart-cluster.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
+sudo systemctl status docker
+sudo systemctl status marathon
+sudo systemctl status mesos-slave
+sudo systemctl status mesos-master
+sudo systemctl status zookeeper
+
 sudo service marathon stop
 sudo service mesos-slave stop
 sudo service docker stop
 sudo service mesos-master stop
+
+/usr/share/zookeeper/bin/zkCli.sh
+rmr /mesos
+rmr /chronos
+rmr /marathon
+quit
+
 sudo service zookeeper stop
 
 sudo service zookeeper start
